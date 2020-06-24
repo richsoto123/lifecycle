0881ceae4da65b90d6d36feca37af8a52ff07ef0
Richard Soto <richsoto123@gmail.com>
2020-06-24 17:20:42 -0400

    added setState loaded and Items: data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      items: [],
+      isLoaded: false,
+    }
+  }
+  increment = () => {
+    
+  }
+
+  componentDidMount(){
+    //put code here 
+    fetch('https://jsonplaceholder.typicode.com/users')
+
+    .then( res  => {
+      //console.log(res);
+      return res.json();
+    })
+    .then(data => {     
+      console.log(data);
+
+      this.setState({
+        isLoaded: true,
+        items: data,      
+      
+      })
+    })
+    
+
+  }
+
+  render(){
+    let { isLoaded, items} = this.state;
+    if (!isLoaded) {
+      return<div>Loading....</div>;
+    } else {
+
+    return (
+      <div>
+        <div>
+          <ul>
+            {}
+          </ul>
+        </div>
+      </div>
+    );
+    }
+    
+  }
 }
-
 export default App;
